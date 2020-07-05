d91a2ca4ccbda5ef38859437cecd384dc36e727c
Arpit Mohapatra <arpit.mohapatra19@gmail.com>
2020-07-05 16:33:32 +0530

    Updates

---
+++ precache-manifest.3c23b148ed9db1644eacc7417c0d8085.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4f35992e9c68dfad0bcee6ec6f480a",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "e037b3cebefa1da4b756",
+    "url": "/covid-tracker/static/css/main.95c62424.chunk.css"
+  },
+  {
+    "revision": "5ef879d9b76b3d4daed7",
+    "url": "/covid-tracker/static/js/2.6d09f51b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.6d09f51b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e037b3cebefa1da4b756",
+    "url": "/covid-tracker/static/js/main.094dee69.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/corona.d7265326.png"
+  }
+]);
